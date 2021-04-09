aac43433bb65073096a25645014c71998e8f357d
georgia <georgia6954@gmail.com>
2021-04-09 23:27:25 +0900

    commit

--- py/changes.py
+++ py/changes.py
@@ -1,18 +1,12 @@
 n = int(input())
 a = ["1","2","3","4","5","6"]
 
-t = len(str(n))
-if t >= 3:
-  o = "1" + ("0" * (t-3))
-  n %= int(o)
+if n > 30:
   n %= 30
 
 i = 0
-while(True):
+for i in range(n):
   tmp = (i % 5) + 1
   a[tmp-1], a[tmp] = a[tmp], a[tmp-1]
-  i += 1
-  if n == i:
-    break
 
 print("".join(a))
