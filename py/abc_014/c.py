80d97bc59e00c2f1b7fd34c07d93401344f1f3a7
georgia <georgia6954@gmail.com>
2021-05-01 23:55:02 +0900

    commit

--- py/abc_014/c.py
+++ py/abc_014/c.py
@@ -1,9 +1,35 @@
 n = int(input())
 arr = []
 
-max_t = 0
-min_t = 999999999
+a = 0
+b = 999999999999999
 for _ in range(n):
-  tmp = input().split()
-  arr.append()
+  tmp = list(map(int, input().split()))
+  a = max(a, tmp[1])
+  b = min(b, tmp[0])
+  arr.append(tmp)
 
+ruiseki = [0] * (a + 2)
+
+for x in arr:
+  ruiseki[x[0]] += 1
+  ruiseki[x[1]+1] -= 1
+
+tmp = 0
+for i in range(len(ruiseki)):
+  ruiseki[i] += tmp
+  tmp = ruiseki[i]
+
+print(max(ruiseki))
+# res = 0
+# count = 0
+# for i in range(min_t, max_t+1):
+#   tmp = 0
+#   for x in arr:
+#     if i in x:
+#       tmp += 1
+#   if count <= tmp:
+#     count = tmp
+#     res = i
+
+# print(res+1)
