e632166b519e55f9ecd107dce32c3477c4e7f95a
ns <georgia6954@gmail.com>
2021-08-15 18:47:23 +0900

    c

---
+++ c++/abc_075/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  int H,W; cin >> H >> W;
+  string S; cin >> S;
+
+  // for (int i = 0; i < H; i++) {
+  //   for (int j = 0; j < W; j++) {
+  //     S[i-1][j-1];
+  //     S[i][j-1];
+  //     S[i+1][j-1];
+
+  //     S[i-1][j];
+  //     S[i][j];
+  //     S[i+1][j];
+
+  //     S[i-1][j+1];
+  //     S[i][j+1];
+  //     S[i+1][j+1];
+
+  //   }
+  // }
+}
